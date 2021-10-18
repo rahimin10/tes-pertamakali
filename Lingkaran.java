fae4fb50fe7b57f748e0979ed9d761c61917b6e7
Rahimin <90886079+rahimin10@users.noreply.github.com>
2021-10-18 12:15:31 +0700

    Create Lingkaran.java

---
+++ Lingkaran.java
@@ -0,0 +1,11 @@
+class Lingkaran 
+{
+ // method menghitung luas dengan jari-jari
+ float luas(float r){
+ return (float) (Math.PI * r * r);
+ }
+ // method menghitung luas dengan diameter
+ double luas(double d){
+ return (double) (1/4 * Math.PI * d);
+ }
+}
